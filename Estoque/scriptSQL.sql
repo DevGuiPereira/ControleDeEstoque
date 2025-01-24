75760292e94bb64ef6ac6114af8ecbb1a5624195
Guilherme Reis Pereira <guilhermerp2004@gmail.com>
2025-01-24 09:54:55 -0300

    ScriptSQL
    Feito os comandos para criação do banco de dados local

--- Estoque/scriptSQL.sql
+++ Estoque/scriptSQL.sql
@@ -1,23 +1,15 @@
-CREATE DATABASE controle_Estoque
+CREATE DATABASE estoque
 
-USE controle_Estoque;
+USE estoque;
 
 CREATE TABLE produtos (
-    idProduto INT AUTO_INCREMENT PRIMARY KEY,
+    idprodutos INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(100),
-    preco DECIMAL(10, 2) NOT NULL,
+    preco DOUBLE NOT NULL,
     quantidade INT NOT NULL
 );
 
-select * from produtos
+DESCRIBE produtos
 
-INSERT INTO produtos (nome, descricao, preco, quantidade)
-VALUES ('teste', 'teste teste', 10, 1)
-
-SHOW GRANTS FOR 'root'@'localhost';
-
-GRANT ALL PRIVILEGES ON controle_estoque.* TO 'root'@'localhost' IDENTIFIED BY 'Guirp007007!';
-FLUSH PRIVILEGES;
-
-SHOW DATABASES;
+SELECT * FROM produtos
